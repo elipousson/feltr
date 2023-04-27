9a9f7eaa603e729ecfada78ce3822553a0439be8
Eli Pousson <eli.pousson@gmail.com>
2023-04-27 11:24:50 -0400

    test: add read_felt_raster test

---
+++ tests/testthat/test-read_felt_raster.R
@@ -0,0 +1,12 @@
+test_that("read_felt_raster works", {
+  raster_image <-
+    read_felt_raster(
+      "https://felt.com/map/feltr-sample-map-read-felt-raster-oiinodTbT79BEueYdGp1aND",
+      "https://tile.loc.gov/image-services/iiif/service:gmd:gmd370:g3700:g3700:ct003955/full/pct:12.5/0/default.jpg"
+    )
+
+  expect_s4_class(
+    raster_image,
+    "SpatRaster"
+  )
+})
