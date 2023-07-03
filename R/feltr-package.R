8ad622080c44963eddd7a34196e82d584afecf59
Eli Pousson <eli.pousson@gmail.com>
2023-07-02 23:48:31 -0400

    refactor: add lifecycle to Imports

--- R/feltr-package.R
+++ R/feltr-package.R
@@ -4,5 +4,6 @@
 ## usethis namespace: start
 #' @import rlang
 #' @importFrom cli cli_abort cli_progress_message cli_alert_success cli_alert_danger
+#' @importFrom lifecycle deprecated
 ## usethis namespace: end
 NULL
